d9b09bae7e4e9530c977341ff03805c5fa4b82dd
Simon Cho <simonmcho@gmail.com>
2020-09-30 15:08:57 -0700

    Day two exercise

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # javascript-practice
-practice for improving on javascript
+Lewis' practice for improving on javascript
+
+The purpose of this repository is for Lewis to understand the concept of javascript programming. It is more important to understand than get the assignment answers correct.
+
+Once a week, Simon will teach a concept and go over some programming techniques. For the rest of the week, it is expected that Lewis will practice on his own with the given assignemnts.
+
+## Good To Knows
+• Every github repository should have a `README.md` file. It shows up as the default file of "descriptions" for the repository    
+• Below are some good to knows for commands    
+
+### Mac Terminal command lines (On windows, you can use git bash)
+https://gist.github.com/poopsplat/7195274
+
+### Git commands
+https://education.github.com/git-cheat-sheet-education.pdf
+
+General steps for pushing to git:
+1. `git status` to see what files have been changed locally    
+2. `git add [files-to-add-here]` (eg. `git add .` for add all files to be prepped for commit. `git add test.js` for just `test.js` file to be prepped for commit)    
+3. `git commit -m "[some-message-here]"` to commit all the files you added via `git add`. It is now ready to be pushed    
+4. `git push` to push all the committed files to remote git repository    
+** You can always run `git status` to see the status of your file commits
---
+++ dayTwo.js
@@ -0,0 +1,63 @@
+// First, some review/practice.
+
+// 1. Create a function called "calculateTwoNumbers" (Already done for you)
+// 2. This function should accept two parameters that are number
+// 3. This function should accept a third parameter that accepts a string
+
+// When calling the function calculateTwoNumbers(8, 2, 'add'), the result should be 10
+// When calling the function calculateTwoNumbers(8, 2, 'subtract), the result should be 6
+// When calling the function calculateTwoNumbers(8, 2, 'multiply'), the result should be 16
+// When calling the function calculateTwoNumbers(8, 2, 'divide'), the result should be 4
+// When the string value is none of the 4 values shown above, return string value "Please enter a proper calculation method"
+
+// Hint: Use if else statements. Think about all 5 conditions (add, subtract, multiply, divide, bad method)
+
+/*
+When you run `node dayTwo.js` in your git bash, you should have a result that looks like this:
+
+Adding two numbers resulted in 10: true
+------------------------------------------------------------------------------
+Subtracting two numbers resulted in 6: true
+------------------------------------------------------------------------------
+Multiplying two numbers resulted in 16: true
+------------------------------------------------------------------------------
+Dividing two numbers resulted in 4: true
+------------------------------------------------------------------------------
+Entering in bad method resulted in the response Please enter a proper calculation method: true
+------------------------------------------------------------------------------ 
+*/
+
+// Add your code below...
+function calculateTwoNumbers() {
+}
+
+// Do not touch code below...
+const actualAdd = calculateTwoNumbers(8, 2, 'add')
+const expectedAdd = 10
+const addAnswer = actualAdd === expectedAdd
+console.log(`Adding two numbers resulted in ${expectedAdd}: ${addAnswer}`)
+console.log('------------------------------------------------------------------------------')
+
+const actualSubtract = calculateTwoNumbers(8, 2, 'subtract')
+const expectedSubtract = 6
+const subtractAnswer = actualSubtract === expectedSubtract
+console.log(`Subtracting two numbers resulted in ${expectedSubtract}: ${subtractAnswer}`)
+console.log('------------------------------------------------------------------------------')
+
+const actualMultiply = calculateTwoNumbers(8, 2, 'multiply')
+const expectedMultiply = 16
+const multiplyAnswer = actualMultiply === expectedMultiply
+console.log(`Multiplying two numbers resulted in ${expectedMultiply}: ${multiplyAnswer}`)
+console.log('------------------------------------------------------------------------------')
+
+const actualDivide = calculateTwoNumbers(8, 2, 'divide')
+const expectedDivide = 4
+const divideAnswer = actualDivide === expectedDivide
+console.log(`Dividing two numbers resulted in ${expectedDivide}: ${divideAnswer}`)
+console.log('------------------------------------------------------------------------------')
+
+const actualBadMethod = calculateTwoNumbers(8, 2, 'randomMethod')
+const expectedBadMethod = 'Please enter a proper calculation method'
+const badMethodAnswer = actualBadMethod === expectedBadMethod
+console.log(`Entering in bad method resulted in the response ${expectedBadMethod}: ${badMethodAnswer}`)
+console.log('------------------------------------------------------------------------------')
