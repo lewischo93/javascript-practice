ede58845747a818f5aba59ec7a004bf35f5ff948
lewischo93 <lewischo93@gmail.com>
2020-09-29 20:35:51 -0700

    day One summary finished

---
+++ dayOneSummary.js
@@ -0,0 +1,15 @@
+function multiplyTwoNumbers (valueOne,valueTwo){ 
+    // function name is multiplyTwoNumbers with arguments valueOne and valueTwo
+    return valueOne*valueTwo
+    // return the product of valueOne and valueTwo
+
+
+}
+console.log(multiplyTwoNumbers(40,87))
+// console.log is like a message to log on the debugging console (github)
+
+function divideTwoNumbers(valueOne, valueTwo){
+    return valueOne/valueTwo
+
+}
+console.log(divideTwoNumbers(40, 10))
--- index.js
+++ index.js
@@ -9,43 +9,62 @@ const testFirstExpected = 'Hello world!'
 const testFirstActual = getString(testFirstExpected)
 
 // Second Exercise - Write a function below that takes TWO number parameters, adds them, and returns the added value
-function addTwoNumbers() {
+function addTwoNumbers(x,y) {
   // Your code here...feel free to add parameters in the paranthesis
+    return x+y
 }
 
 const testSecondExpected = 15
-const testSecondActual = addTwoNumbers(5, 10)
+const testSecondActual = addTwoNumbers(2, 7)
+function subtractThreeNumbers(x,y,z) {
+    return x-y-z
+}
+console.log(subtractThreeNumbers(10,2,4))
+
+// if (hour < 18) {
+//     greeting = "Good day";
+//   } else {
+//     greeting = "Good evening";
+//   }
 
 // Third Exercise - Write a function below that takes TWO number parameters, and ONLY returns the value if the added value is greater than 20
-function addTwoNumbersGreaterThanTwenty() {
+function addTwoNumbersGreaterThanTwenty(numberone,numbertwo) {
   // Your code here...feel free to add parameters in the paranthesis
-}
+    const sum = numberone+numbertwo
+        if (sum > 20) {
+         return sum
+        } else {
+            return 0
+        }
 
+}
+console.log('NEWANSWERBELOW')
+console.log(addTwoNumbersGreaterThanTwenty(5, 10))
 const testThirdExpected = 25
 const testThirdActual = addTwoNumbersGreaterThanTwenty(15, 10)
 
-console.log('Tests for practice.')
-console.group()
-console.log('------------------------------------------------------------------------------------------------------------')
-console.log(`First Test::Expect getString to return ${testFirstExpected}`)
-console.log(`Test passed: ${testFirstActual === testFirstExpected}`)
-console.log('------------------------------------------------------------------------------------------------------------')
-console.groupEnd()
-console.group()
-console.log('------------------------------------------------------------------------------------------------------------')
-console.log(`Second Test::Expect addTwoNumbers to return ${testSecondExpected}`)
-console.log(`Test passed: ${testSecondActual} --- ${testSecondExpected}`)
-console.log('------------------------------------------------------------------------------------------------------------')
-console.groupEnd()
-console.group()
-console.log('------------------------------------------------------------------------------------------------------------')
-console.log(`Third Test::Expect addTwoNumbersGreaterThanTwenty to return ${testThirdExpected}`)
-console.log(`Test passed: ${testThirdActual} --- ${testThirdExpected}`)
-console.log('------------------------------------------------------------------------------------------------------------')
-console.groupEnd()
-console.group()
-console.log('------------------------------------------------------------------------------------------------------------')
-console.log(`Third Test::Expect addTwoNumbers to return ${testSecondExpected}`)
-console.log(`Test passed: ${testSecondActual} --- ${testSecondExpected}`)
-console.log('------------------------------------------------------------------------------------------------------------')
-console.groupEnd()
+// console.log('Tests for practice.')
+// console.group()
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.log(`First Test::Expect getString to return ${testFirstExpected}`)
+// console.log(`Test passed: ${testFirstActual === testFirstExpected}`)
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.groupEnd()
+// console.group()
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.log(`Second Test::Expect addTwoNumbers to return ${testSecondExpected}`)
+// console.log(`Test passed: ${testSecondActual} --- ${testSecondExpected}`)
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.groupEnd()
+// console.group()
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.log(`Third Test::Expect addTwoNumbersGreaterThanTwenty to return ${testThirdExpected}`)
+// console.log(`Test passed: ${testThirdActual} --- ${testThirdExpected}`)
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.groupEnd()
+// console.group()
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.log(`Third Test::Expect addTwoNumbers to return ${testSecondExpected}`)
+// console.log(`Test passed: ${testSecondActual} --- ${testSecondExpected}`)
+// console.log('------------------------------------------------------------------------------------------------------------')
+// console.groupEnd()
