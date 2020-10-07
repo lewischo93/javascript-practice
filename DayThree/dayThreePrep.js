054da938139d40ea98a8bf1c4f53964bcc91bbc1
lewischo93 <lewischo93@gmail.com>
2020-10-07 14:45:32 -0700

    [attempt 1]

--- DayThree/dayThreePrep.js
+++ DayThree/dayThreePrep.js
@@ -3,3 +3,13 @@
 // Conditions:
 // 1. If the sum of the 2 params is an even number, return string 'EVEN SUM'
 // 2. If the sum of the 2 params is an odd number, return string 'ODD SUM'
+
+function twoVariables (numberOne, numberTwo){
+    const sum = numberOne+numberTwo
+    if (sum = evenNumber) {
+    return 'EVEN SUM'
+    } else if (sum = oddNumber) {
+        return 'ODD SUM'
+    }
+    }
+    
