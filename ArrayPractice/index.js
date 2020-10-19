21cd9d6c5bc01fc183b1215f7c6383406928ac20
Simon Cho <simonmcho@gmail.com>
2020-10-18 21:00:36 -0700

    ArrayPractice

---
+++ ArrayPractice/index.js
@@ -0,0 +1,25 @@
+// Return second item of array passed in
+function getSecondItem(arr) {
+  // code here...
+}
+
+// Return second to last item of array passed in
+function getSecondToLastItem(arr) {
+  // code here...  
+}
+
+// Return sum of the last element of both arrays passed in
+// Eg. If first parameter is [1, 2, 3] and second parameter is [5, 6, 7]
+// the function should return 10, because it's 3 +7
+// Another eg. [2, 4, 5] and [1, 5, 10] passed in should return 15 (because it's 5 + 10)
+function getSumOfBothLastElements(arrOne, arrTwo){
+  // code here...
+}
+
+// Review...create a function that takes one parameter
+// If bigger than 20, return "BIG NUMBER!"
+// If smaller than 20, return "SMALL..."
+// If parameter is equal to 20, return the passed in parameter
+function getValue(num){
+  // code here...
+}
