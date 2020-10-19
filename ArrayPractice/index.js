fe310fdc296fb0cb3421799c407defa51bd2c5d5
lewischo93 <lewischo93@gmail.com>
2020-10-18 22:29:37 -0700

    attempt one

--- ArrayPractice/index.js
+++ ArrayPractice/index.js
@@ -1,19 +1,39 @@
+
 // Return second item of array passed in
 function getSecondItem(arr) {
-  // code here...
+  const firstItem = arr[0];
+  const secondItem = arr[firstItem+1];
+  return secondItem
 }
 
 // Return second to last item of array passed in
 function getSecondToLastItem(arr) {
   // code here...  
+  const length = arr.length
+  const secondLastItem = arr[length-2];
+  return secondLastItem
+
 }
 
+// Lewis note: I think I got it. Tried it in console and it seemed to work. Seems way too lengthy or is it okay?
 // Return sum of the last element of both arrays passed in
 // Eg. If first parameter is [1, 2, 3] and second parameter is [5, 6, 7]
 // the function should return 10, because it's 3 +7
 // Another eg. [2, 4, 5] and [1, 5, 10] passed in should return 15 (because it's 5 + 10)
-function getSumOfBothLastElements(arrOne, arrTwo){
+function getSumOfBothLastElements(arrOne, arrTwo) {
+  arrOne = [1, 2, 3, 4, 5, 6, 7]
+  arrTwo = [20, 30, 40, 60]
+
+  const lengthofArrOne = arrOne.length
+  const lastItemofArrOne = arrOne[lengthofArrOne-1];
+
+  const lengthofArrTwo = arrTwo.length
+  const lastItemofArrTwo = arrTwo[lengthofArrTwo-1];
+
+  const sumOfLastElements =  lastItemofArrOne+lastItemofArrTwo
+  return sumOfLastElements
   // code here...
+
 }
 
 // Review...create a function that takes one parameter
@@ -21,5 +41,13 @@ function getSumOfBothLastElements(arrOne, arrTwo){
 // If smaller than 20, return "SMALL..."
 // If parameter is equal to 20, return the passed in parameter
 function getValue(num){
-  // code here...
+    let updatedNum = num
+    if (num > 20) {
+      updatedNum = 'BIG NUMBER!'
+    } else if (num < 20){
+      updatedNum = 'SMALL....'
+    } else {
+      updatedNum = num
+    }
+    return updatedNum
 }
